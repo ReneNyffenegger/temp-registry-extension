7bed1798739b4ebdbae4e808b24927a16609cf33
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-08-16 17:49:08 +0200

    n/a

---
+++ before-ftype/HKCR-.pl
@@ -0,0 +1,8 @@
+
+HKEY_CLASSES_ROOT\.pl
+    (Default)    REG_SZ    PerlScript
+    PerceivedType    REG_SZ    text
+
+HKEY_CLASSES_ROOT\.pl\PersistentHandler
+    (Default)    REG_SZ    {5e941d80-bf96-11cd-b579-08002b30bfeb}
+
---
+++ before-ftype/HKLM-Software-Classes-.pl
@@ -0,0 +1,8 @@
+
+HKEY_LOCAL_MACHINE\Software\Classes\.pl
+    PerceivedType    REG_SZ    text
+    (Default)    REG_SZ    PerlScript
+
+HKEY_LOCAL_MACHINE\Software\Classes\.pl\PersistentHandler
+    (Default)    REG_SZ    {5e941d80-bf96-11cd-b579-08002b30bfeb}
+
