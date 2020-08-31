75754b35603baa2e3095b30f60946092c9a9a8c0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-08-31 18:52:39 +0200

    n/a

---
+++ try-to-assign/try-to-assign.ps1
@@ -0,0 +1,69 @@
+$null = new-psDrive -name HKUS -psProvider registry -root HKEY_USERS
+
+
+$ext   = '.pl'
+$ftype = 'PerlScript'
+$exe   = 'C:\Users\Rene\StrawberryPerl\perl\bin\perl.exe'
+
+$userSid = 'S-1-5-21-1986187950-1154198019-2155599821-1001'
+
+
+
+#
+#   ------------ Remove keys ---------------
+#
+
+function removeRegistryKey($key) {
+
+  if (test-path $key) {
+     write-host "Removing $key"
+     $null = remove-item -recurse -force $key
+  }
+
+}
+
+removeRegistryKey "HKLM:\Software\Classes\$ext"
+
+# if (test-path ) {
+#   remove-item -recurse -force "HKLM:\Software\Classes\$ext"
+# }
+
+
+removeRegistryKey "HKLM:\Software\Classes\$ftype" 
+
+removeRegistryKey "HKUS:\$userSid\Software\Classes\$ext"
+
+# if (test-path ) {
+#   remove-item -recurse -force "HKLM:\Software\Classes\$ext"
+# }
+
+
+removeRegistryKey "HKUS:\$userSid\Software\Classes\$ftype" 
+
+# if (test-path "HKLM:\Software\Classes\$ftype") {
+#   remove-item -recurse -force "HKLM:\Software\Classes\$ftype"
+# }
+ 
+
+removeRegistryKey "HKUS:\$userSid\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext" 
+
+# if (test-path "HKUS:\$userSid\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext") {
+#   remove-item -recurse -force "HKUS:\$userSid\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext"
+# }
+
+
+
+#
+#   ------------ Create keys ----------------
+#
+
+# new-item -path "HKUS:\$userSid\Software\Classes\$ext"
+$null = new-item -path "HKUS:\$userSid\Software\Classes\$ext" -value $ftype
+
+$null = new-item -path "HKUS:\$userSid\Software\Classes\$ftype"
+$null = new-item -path "HKUS:\$userSid\Software\Classes\$ftype\shell"
+$null = new-item -path "HKUS:\$userSid\Software\Classes\$ftype\shell\Open"
+$null = new-item -path "HKUS:\$userSid\Software\Classes\$ftype\shell\Open\Command"
+
+# echo                                                                                          "`"$exe`" `"%1`""
+  $null = set-item -path "HKUS:\$userSid\Software\Classes\PerlScript\shell\Open\Command" -value "`"$exe`" `"%1`""
