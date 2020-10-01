ad22094d20e87525ef60f2bd7aa805bd498f86d2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-10-01 15:01:28 +0200

    n/a

--- try-to-assign/show-reg-values.ps1
+++ try-to-assign/show-reg-values.ps1
@@ -1,13 +1,18 @@
 $null    =  new-psDrive -name HKUS -psProvider registry -root HKEY_USERS
 
-  $ext     = '.pl'
+# $ext     = '.pl'
 # $ftype   = '.pl_auto_file'
-  $ftype   = 'PerlScript'
+# $ftype   = 'PerlScript'
 
 # $ext     = '.jpg'
 # $ftype   = 'IrfanView.jpg'
+#
+  $ext     = '.png'
+# $ftype   = 'IrfanView.jpg'
 
 $userSid = 'S-1-5-21-1986187950-1154198019-2155599821-1001'
+# $reg_root_user="HKUS:\$userSid" # used if no admin access possible
+  $reg_root_user="HKCU:"          # used othersiwe no admin access possible
 
 function showRegValue($key, $valName) {
   if (test-path $key) {
@@ -18,10 +23,15 @@ function showRegValue($key, $valName) {
   }
 }
 
-showRegValue "HKUS:\$userSid\Software\Classes\$ext"                      ""
-showRegValue "HKUS:\$userSid\Software\Classes\$ftype\shell\open\Command" ""
+$ftyle = get-childItem "$reg_root_user\Software\Classes\$ext"
+
+# showRegValue "HKUS:\$userSid\Software\Classes\$ext"                      ""
+# showRegValue "HKUS:\$userSid\Software\Classes\$ftype\shell\open\Command" ""
+showRegValue "$reg_root_user\Software\Classes\$ext"                      ""
+showRegValue "$reg_root_user\Software\Classes\$ftype\shell\open\Command" ""
 
 showRegValue "hklm:\Software\Classes\$ext"                      ""
 showRegValue "hklm:\Software\Classes\$ftype\shell\open\Command" ""
 
-showRegValue "HKUS:\$userSid\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice" "ProgId"
+# showRegValue "HKUS:\$userSid\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice" "ProgId"
+# showRegValue "HKUS:\$userSid\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice" "ProgId"
