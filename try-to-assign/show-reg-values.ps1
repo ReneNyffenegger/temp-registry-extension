29885190685df7b52933080e4e9a67cacb90b131
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-04-10 10:25:08 +0200

    n/a

--- try-to-assign/show-reg-values.ps1
+++ try-to-assign/show-reg-values.ps1
@@ -1,19 +1,19 @@
 $null    =  new-psDrive -name HKUS -psProvider registry -root HKEY_USERS
 
-# $ext     = '.pl'
+  $ext     = '.pl'
 # $ftype   = '.pl_auto_file'
-# $ftype   = 'PerlScript'
+  $ftype   = 'PerlScript'
 
 # $ext     = '.jpg'
 # $ftype   = 'IrfanView.jpg'
 #
-  $ext     = '.tiff'
+# $ext     = '.tiff'
 # $ftype   = 'IrfanView.tif'
-  $ftype   = 'IrfanView'
+# $ftype   = 'IrfanView'
 
-$userSid = 'S-1-5-21-1986187950-1154198019-2155599821-1001'
-  $reg_root_user="HKUS:\$userSid" # used if no admin access possible
-# $reg_root_user="HKCU:"          # used otherwise no admin access possible
+  $ext     = '.pdf'
+# $ftype   = '.pl_auto_file'
+  $ftype   = 'pdf_tq84'
 
 function showRegValue($root, $key, $valName) {
 
@@ -27,22 +27,37 @@ function showRegValue($root, $key, $valName) {
   }
 }
 
-$ftyle = get-childItem "$reg_root_user\Software\Classes\$ext"
+function showMultipleRegValues($root) {
+
+   
+   write-host ''
+   write-host "  $root"
+   showRegValue  $root "\Software\Classes\$ext"                      ""
+   showRegValue  $root "\Software\Classes\$ftype\shell\open\Command" ""
+   showRegValue  $root "\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice" "ProgId"
+
+}
+
+
+$userSid = 'S-1-5-21-1986187950-1154198019-2155599821-1001'
+  $reg_root_user="HKUS:\$userSid" # used if no admin access possible
+# $reg_root_user="HKCU:"          # used otherwise no admin access possible
+
+# $ftyle = get-childItem "$reg_root_user\Software\Classes\$ext"
+
 
 # showRegValue "HKUS:\$userSid\Software\Classes\$ext"                      ""
 # showRegValue "HKUS:\$userSid\Software\Classes\$ftype\shell\open\Command" ""
-write-host ''
-write-host "  $reg_root_user"
-showRegValue  $reg_root_user "\Software\Classes\$ext"                      ""
-showRegValue  $reg_root_user "\Software\Classes\$ftype\shell\open\Command" ""
-showRegValue  $reg_root_user "\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice" "ProgId"
+#
+showMultipleRegValues 'hkcu:'
+showMultipleRegValues 'hklm:'
 
-write-host ''
-write-host '  hklm:'
-
-showRegValue "hklm:" "\Software\Classes\$ext"                      ""
-showRegValue "hklm:" "\Software\Classes\$ftype\shell\open\Command" ""
-showRegValue "hklm:" "\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice"          "ProgId"
+# write-host ''
+# write-host '  hklm:'
+# 
+# showRegValue "hklm:" "\Software\Classes\$ext"                      ""
+# showRegValue "hklm:" "\Software\Classes\$ftype\shell\open\Command" ""
+# showRegValue "hklm:" "\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$ext\UserChoice"          "ProgId"
 
 write-host ''
 
